a320516160e9992a75080abc72b531b47bb2c5db
snkdrel <ces.zop@gmail.com>
2022-06-27 14:02:57 -0500

    Create main objects in js

---
+++ main.js
@@ -0,0 +1,18 @@
+const gameBoard = (() => {
+    const gameBoardArray = [
+        ['x', 'x', 'o'],
+        ['o', 'x', 'o'],
+        ['o', 'o', 'x']
+    ];
+    return {gameBoardArray};
+})();
+
+const playerFactory = () => {
+    // attributes
+    // methods
+    return {};
+};
+
+const displayController = (() => {
+    //
+})();
