326d07d541b82c2038effcfcb94c4b5710738411
snkdrel <ces.zop@gmail.com>
2022-06-27 14:57:26 -0500

    Add turns functionality
    Each time a cell is clicked, a new mark is drawn in it, according to
    the current player.

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 const gameBoard = (() => {
-    const gameBoardArray = [
-        ['x', 'x', 'o'],
-        ['o', 'x', 'o'],
-        ['o', 'o', 'x']
+    let gameBoardArray = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
     ];
     const displayBoard = () => {
-        const table = document.querySelectorAll('td');
-        table.forEach((cell) => {
-            cell.textContent = 'X';
+        cells.forEach((cell) => {
+            const x = cell.getAttribute('class').slice(1,2);
+            const y = cell.getAttribute('class').slice(2);
+            cell.textContent = gameBoardArray[x][y];
         });
     };
-    return {gameBoardArray, displayBoard};
+    const newMark = (coordinates, mark) => {
+        const x = coordinates.slice(0,1);
+        const y = coordinates.slice(1);
+        gameBoardArray[x][y] = mark;
+        displayBoard();
+    };
+    return {gameBoardArray, newMark};
 })();
 
 const playerFactory = (mark) => {
@@ -20,6 +27,25 @@ const playerFactory = (mark) => {
 const gameController = (() => {
     const player1 = playerFactory('X');
     const player2 = playerFactory('O');
+    let turnsPassed = 0;
+
+    const makeTurn = (cellClass) => {
+        let currentPlayer;
+        if(turnsPassed % 2 == 0){
+            currentPlayer = player1;
+        }else{
+            currentPlayer = player2;
+        }
+        gameBoard.newMark(cellClass.slice(1), currentPlayer.mark);
+        
+        turnsPassed++;
+    };
+    return{makeTurn};
 })();
 
-gameBoard.displayBoard();
+const cells = document.querySelectorAll('td');
+cells.forEach((c) => {
+    c.addEventListener('click', () => {
+        gameController.makeTurn(c.getAttribute('class'));
+    });
+});
